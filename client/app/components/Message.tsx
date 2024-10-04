8e29f6fa5ba36be522cd06c71335e48924b66e21
Viresh Mistry <vireshkumarmistry@gmail.com>
2024-10-04 16:07:16 +0530

    Update message box component

--- client/app/components/Message.tsx
+++ client/app/components/Message.tsx
@@ -1,4 +1,6 @@
 import { marked } from "marked";
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faRobot, faUser } from '@fortawesome/free-solid-svg-icons';
 
 interface MessageProps {
   message: {
@@ -14,16 +16,24 @@ const Message = ({ message }: MessageProps) => {
 
   return (
     <div className={`p-2 ${message.sender === 'user' ? 'text-right' : 'text-left'}`}>
-      <div
-        className={`inline-block px-4 py-2 rounded ${
-          message.sender === 'user' ? 'bg-blue-500 text-white' : 'bg-gray-200 text-black'
-        }`}
-      >
-        {message.isHtml ? (
-          <div dangerouslySetInnerHTML={{ __html: formattedText }} />
-        ) : (
-          message.text
+      <div className={`flex items-center ${message.sender === 'user' ? 'justify-end' : 'justify-start'}`}>
+        {message.sender === 'bot' && (
+          <FontAwesomeIcon icon={faRobot} className="mr-2 text-gray-200" />
         )}
+        {message.sender === 'user' && (
+          <FontAwesomeIcon icon={faUser} className="mr-2 text-blue-500" />
+        )}
+        <div
+          className={`inline-block px-4 py-2 rounded ${
+            message.sender === 'user' ? 'bg-blue-500 text-white' : 'bg-gray-200 text-black'
+          }`}
+        >
+          {message.isHtml ? (
+            <div dangerouslySetInnerHTML={{ __html: formattedText }} />
+          ) : (
+            message.text
+          )}
+        </div>
       </div>
     </div>
   );
